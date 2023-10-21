f7b6de1dcb4287997c2495ae7e92ce1d29b7c8f5
Urkagurka <143172181+Urkagurka@users.noreply.github.com>
2023-10-21 17:38:44 +0200

    Add project files.

---
+++ Uppgift 4.8 inl/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Uppgift_4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en lång sträng");
+            string Lång = Console.ReadLine();
+            Console.WriteLine("Skriv in en kort sträng på 2 bokstäver");
+            string Kort = Console.ReadLine();
+
+            string hittade = "";
+
+            for (int i = 0; i < Kort.Length; i++)
+            {
+                char hitta = Kort[i];
+
+                for (int j = 0; j < Lång.Length; j++)
+                {
+                    char leta = Lång[j];
+                    if (hitta == leta)
+                    {
+                        hittade = hittade + hitta;
+                        break;
+                    }
+                }
+            }
+            if (hittade == Kort)
+            {
+                Console.WriteLine("Bokstäverna finns");
+            }
+            else
+            {
+                Console.WriteLine("Alla fanns inte");
+            }
+        }
+    }
+}
